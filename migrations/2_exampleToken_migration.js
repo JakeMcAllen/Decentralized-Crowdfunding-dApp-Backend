ba6a20434833aeaaccef32c16fe92d9b8a8205e0
Giacomo Corrias <giacomo.corrias7@gmail.com>
2020-08-24 12:11:02 +0200

    [refactoring] removed example contract; add keepers and startup files; update default env

--- migrations/2_exampleToken_migration.js
+++
@@ -1,6 +0,0 @@
-const ExampleToken = artifacts.require("ExampleToken");
-const initialFixedSupply = 1000000; // ExampleToken contract constructor argument.
-
-module.exports = function(deployer) {
-  deployer.deploy(ExampleToken, initialFixedSupply);
-};
--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,18 @@
-// Dummy Smart Contract interaction.
 require('dotenv').config()
-import Web3 from 'web3'
+const Web3 = require('web3')
 
 async function main () {
     // A web3 provider comes from truffle. 
     // Through `truffle exec` command we execute this function using that web3 provider.
     // NB. The web3 provider comes from --network parameter specified (development or testnet).
-    if (!web3) {
+    if (!web3) {
         throw Error("Something went wrong with the web3 provider. Did you have run the command using truffle exec?");
     }
-
+    
     // Initialize Web3 instance with the truffle provider.
     const web3Instance = new Web3(web3.currentProvider)
 
-    // Retrieve the name of the current network.
-    const network = await web3Instance.eth.net.getNetworkType()
-    console.log(network) // must be private
+    // Your script code here.
 }
 
 // Required by `truffle exec`
